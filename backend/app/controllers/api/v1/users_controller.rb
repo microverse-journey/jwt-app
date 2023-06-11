2fa5a5df22ae77b38e4039487ca86cab32abf252
mrprotocoll <lekanvgbg@gmail.com>
2023-06-11 17:22:20 +0100

    Setuo authentication

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,21 @@
 class Api::V1::UsersController < ApplicationController
+    def create
+        user = User.new(user_params)
+        if user.save
+            token = issue_token(user)
+            render json: {user: UserSerializer.new(user), token: token}
+        else
+            if user.error.messages
+                render json: {error: user.errors.messages}
+            else
+                render json:  {error: "User could not be created. Try again"}
+            end
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password)
+    end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  namespace :api do
+    namespace :v1 do
+      post "/signup", to: "users#create"
+      post "/login", to: "sessions#create"
+      get "/authorized", to: "sessions#show"
+      get "/dashboard", to: "users#show"
+    end
+  end
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "user#index"
 end
